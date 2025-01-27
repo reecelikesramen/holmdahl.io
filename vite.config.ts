50224f04126fadec87a38d3501530b62b45dbb96
reecelikesramen <3149587+reecelikesramen@users.noreply.github.com>
2025-01-27 14:33:39 -0600

    Simplified the vite build process - Scripts don't need conditionals, they just need to reference the absolute source path - Simplified output, no more dist/assets, all js, css assets go to their respective extname folder in dist - vite config is a bit more resilient by using .tmp directory for copied public dir - referencing ts and tsx files directly in templates now - raytracer and main js still work

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,58 @@
-import * as fs from "fs/promises"
-import { globSync } from "glob"
-import { defineConfig } from "vite"
-import wasm from "vite-plugin-wasm"
-import topLevelAwait from "vite-plugin-top-level-await"
-import { resolve } from "node:path"
-import preact from "@preact/preset-vite"
+import * as fs from "fs/promises";
+import { globSync } from "glob";
+import { defineConfig, Rollup } from "vite";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
+import { resolve } from "node:path";
+import preact from "@preact/preset-vite";
 
 const movePublicPlugin = {
   name: "move-public",
+  // copy dist/public/ to dist/.tmp/
+  // remove dist/public/
+  // copy dist/.tmp/* to dist/
+  // remove dist/.tmp/
   closeBundle: async () => {
-    await fs.cp(resolve("dist/public"), resolve("dist"), { recursive: true })
-    await fs.rm(resolve("dist/public"), { recursive: true })
+    await fs.cp(resolve("dist/public"), resolve("dist/.tmp"), {
+      recursive: true,
+    });
+    await fs.rm(resolve("dist/public"), { recursive: true });
+    await fs.cp(resolve("dist/.tmp"), resolve("dist"), { recursive: true });
+    await fs.rm(resolve("dist/.tmp"), { recursive: true });
   },
-}
+};
+
+const rollupOutput: Rollup.OutputOptions = {
+  entryFileNames: "js/[name].min.[hash].js",
+  chunkFileNames: "js/[name].min.[hash].js",
+  assetFileNames: (assetInfo) => {
+    if (assetInfo.names?.some((name) => /^.+\.(js|wasm)$/.test(name))) {
+      return "js/[name].min.[hash][extname]";
+    }
+
+    // find the first file extension, otherwise its just assets
+    const extname = assetInfo.names?.[0]?.split(".")?.pop() ?? "assets";
+    return extname + "/[name].min.[hash][extname]";
+  },
+};
 
 export default defineConfig({
   plugins: [wasm(), topLevelAwait(), preact(), movePublicPlugin],
   build: {
     rollupOptions: {
       input: globSync("public/**/*.html"),
+      output: rollupOutput,
     },
   },
-  base: "https://reece.holmdahl.io",
+  worker: {
+    format: "es",
+    rollupOptions: {
+      output: rollupOutput,
+    },
+  },
   resolve: {
     alias: {
       json5: "json5/lib/index.js",
     },
   },
-})
+});
