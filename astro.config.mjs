dc601f3ba745a45eb2a9e2563c54baf6982dc97f
reecelikesramen <3149587+reecelikesramen@users.noreply.github.com>
2025-06-05 00:24:38 -0500

    build needs site base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  // Configure for GitHub Pages deployment
+  site: process.env.GITHUB_PAGES_SITE || 'https://localhost:4321',
+  base: process.env.GITHUB_PAGES_BASE || '/',
+  
   vite: {
       plugins: [tailwindcss()]
 	},
